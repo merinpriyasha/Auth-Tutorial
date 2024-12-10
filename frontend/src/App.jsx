e7117fab9c46f064c0ab79f30ef75f1b76cc1720
merinpriyasha <merinpriyasha@gmail.com>
2024-12-10 21:48:47 +0530

    implementing forget-password and reset-password pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,9 @@ import DashboardPage from "./pages/DashboardPage";
 import LoadingSpinner from "./components/LoadingSpinner";
 
 import EmailVerificationPage from "./pages/EmailVerificationPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authStore";
 import { useEffect } from "react";
@@ -48,7 +51,7 @@ function App() {
     }, [checkAuth]);
     
     if (isCheckingAuth) return <LoadingSpinner />;
-    
+
   return (
     <div className='min-h-screen bg-gradient-to-br
     from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
@@ -72,6 +75,25 @@ function App() {
         </RedirectAuthenticatedUser>} />
 
         <Route path='/verify-email' element={<EmailVerificationPage/>} />
+
+        <Route
+					path='/forgot-password'
+					element={
+						<RedirectAuthenticatedUser>
+							<ForgotPasswordPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+
+<Route
+					path='/reset-password/:token'
+					element={
+						<RedirectAuthenticatedUser>
+							<ResetPasswordPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+
       </Routes>
       <Toaster />
       
